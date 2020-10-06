9a675316acc3a91a2eedfddd6c0dac5b5f1f9a63
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-10-06 15:13:38 +0500

    Fixed ts-jest to use th repo ts config

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,16 +1,16 @@
-const supertest = require('supertest')
-import app from '../api/app'
+import app from "../api/app";
+import supertest from "supertest";
 
-describe('index route', () => {
-  it('it runs', async (done) => {
-    const res = await supertest(app).get('/')
-    expect(res.body).toEqual({ message: 'API is up 🚀' })
-    expect(res.status).toBe(200)
-    done()
-  })
-  it('it returns URL cannot be found', async () => {
-    const res = await supertest(app).get('/server/auth')
-    expect(res.body).toEqual({ message: 'This URL can not be found' })
-    expect(res.status).toBe(404)
-  })
-})
+describe("index route", () => {
+  it("it runs", async (done) => {
+    const res = await supertest(app).get("/");
+    expect(res.body).toEqual({ message: "API is up 🚀" });
+    expect(res.status).toBe(200);
+    done();
+  });
+  it("it returns URL cannot be found", async () => {
+    const res = await supertest(app).get("/server/auth");
+    expect(res.body).toEqual({ message: "This URL can not be found" });
+    expect(res.status).toBe(404);
+  });
+});
