19d5b20ca6a1b24dfa3c4f42c6f42cf8767d4074
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-10-08 13:55:29 +0500

    Workspaces model

---
+++ src/models/Workspace.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const Label = new mongoose.Schema({
+  id: mongoose.Types.ObjectId,
+  name: String,
+  color: String, //hex color code
+});
+
+const Change = new mongoose.Schema({
+  id: mongoose.Types.ObjectId,
+  //   text: String,
+  // what happened // string based on the type of change or a type of possible action
+
+  // what changed  // a reference to the object in the Db, preferably by it's id
+  target: { type: mongoose.Types.ObjectId },
+  subtarget: { type: mongoose.Types.ObjectId, required: false },
+  // who did it  // reference to the user id
+  user: { type: mongoose.Types.ObjectId, ref: "Users" },
+});
+
+const Workspaces = mongoose.model(
+  "Workspace",
+  new mongoose.Schema({
+    id: mongoose.Types.ObjectId,
+    name: String,
+    labels: [Label], //all of labels defined for this workspace
+    users: [{ type: mongoose.Types.ObjectId, ref: "Users" }], //references to all the users
+    admin: { type: mongoose.Types.ObjectId, ref: "Users" },
+    tasks: [{ type: mongoose.Types.ObjectId, ref: "Tasks" }],
+    history: [Change],
+    timestamps: {},
+  })
+);
+
+export default Workspaces;
