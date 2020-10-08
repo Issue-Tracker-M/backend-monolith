c80e74235e0bfdfde9de919150cce8a5f876fed6
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-10-08 14:10:07 +0500

    Fixed missing type annotation

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,6 +1,6 @@
 import db from "../models";
 import mongoose from "mongoose";
-const createUser = (user) => {
+const createUser = (user: any) => {
   return db.Users.create(user).then((userDoc) => {
     console.log("created user");
     return userDoc;
