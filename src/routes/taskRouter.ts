a84e6334ac97a1810826ca3856105492dadbff23
Uzoamaka126 <vanessapeters126@gmail.com>
2020-10-14 08:30:08 +0100

    remove files accidentally pushed to main branch

--- src/routes/taskRouter.ts
+++ src/routes/taskRouter.ts
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-import { createTask, getTasks , editTask } from '../controllers/tasks';
+import { createTask } from '../controllers/tasks';
 
 router.post('/', createTask);
-router.get('/', getTasks);
-router.put('/', editTask);
 
 module.exports = router;
