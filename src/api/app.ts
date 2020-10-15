23940beb008f3e9709d526c58f3a0e842bbfa409
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-15 14:46:49 +0500

    router refactors

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,25 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-const { port, mongoURI } = require('../config/index');
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+const { port, mongoURI } = require("../config/index");
 
-const authRouter = require('../routes/authRouter');
-const taskRouter = require('../routes/taskRouter');
-const workspaceRouter = require('../routes/workspaceRouter');
+const authRouter = require("../routes/authRouter");
+const taskRouter = require("../routes/taskRouter");
+const workspaceRouter = require("../routes/workspaceRouter");
+const apiRouter = express.Router();
+apiRouter.use("/auth", authRouter);
+apiRouter.use("/tasks", taskRouter);
+apiRouter.use("/workspaces", workspaceRouter);
 
 const app = express();
 
-app.set('port', port);
+app.set("port", port);
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 
-app.use('/api/auth', authRouter);
-app.use('/api/task', taskRouter);
-app.use('/api/workspace', workspaceRouter);
-
+app.use("/api", apiRouter);
 mongoose
   .connect(
     mongoURI,
@@ -31,12 +32,12 @@ mongoose
   )
   .catch(console.error);
 
-app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'API is up 🚀' });
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "API is up 🚀" });
 });
 
-app.all('*', (req, res) => {
-  res.status(404).json({ message: 'This URL can not be found' });
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This URL can not be found" });
 });
 
 export default app;
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,6 +1,12 @@
-const router = require('express').Router();
-import createUser from '../controllers/users/createUser';
+import {
+  validateLoginInput,
+  validateRegisterInput,
+} from "../controllers/auth/validation";
+import { register, login } from "../controllers/auth";
+import { getUserByCredential } from "../controllers/auth/middleware";
+const router = require("express").Router();
 
-router.post('/create', createUser);
+router.post("/register", validateRegisterInput, register);
+router.post("/login", validateLoginInput, getUserByCredential, login);
 
 module.exports = router;
