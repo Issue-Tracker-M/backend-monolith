7889fc905a107d75401958c29bcbf99234b08f5b
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-15 14:35:54 +0500

    added JWT secret tot config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const port = process.env.PORT || 5000;
+export const port = process.env.PORT || 5000;
+export const SECRET = process.env.JWT_SECRET || "bloop";
 const { DB_CONNECTION, NODE_ENV, DB_CONNECTION_TEST } = process.env;
-let mongoURI: any = '';
+export let mongoURI = "";
 
-if (NODE_ENV === 'test') {
+if (NODE_ENV === "test" && DB_CONNECTION_TEST) {
   mongoURI = DB_CONNECTION_TEST;
-} else {
+} else if (DB_CONNECTION) {
   mongoURI = DB_CONNECTION;
 }
-
-module.exports = {
-  port,
-  mongoURI,
-};
