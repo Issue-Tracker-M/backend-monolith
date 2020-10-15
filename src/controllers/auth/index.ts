d2148bd65faacb1fa1c75dca35cdb4d601a6bf25
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-15 14:46:35 +0500

    regular register and login enpoints

---
+++ src/controllers/auth/index.ts
@@ -0,0 +1,61 @@
+import { IUser } from "../../models/User";
+import { Request, Response } from "express";
+import User from "../../models/User";
+import generateToken from "../../utils/generateToken";
+import bcrypt from "bcrypt";
+
+/**
+ * Registers a new user with username, email & password. Sends back the new user document & token.
+ * @param req
+ * @param res
+ */
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      first_name,
+      last_name,
+      username,
+      password,
+      email,
+    }: IUser = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      first_name,
+      last_name,
+      username,
+    });
+
+    const user = await newUser.save();
+
+    const token = generateToken(user);
+
+    res.status(201).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export interface loginInput {
+  credential: IUser["username"] | IUser["email"];
+  password: IUser["password"];
+}
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      (req.user as IUser).password
+    );
+    if (validPassword) {
+      const token = generateToken(req.user as IUser);
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to login" });
+  }
+};
---
+++ src/controllers/auth/middleware.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from "express";
+import { SECRET } from "../../config";
+import validateToken from "../../utils/validateToken";
+import User from "../../models/User";
+
+/**
+ * Extracts user id from token, fetches that user and appends the user object to req.user
+ * @param req
+ * @param res
+ * @param next
+ */
+export const checkToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const token = req.body;
+    const decodedToken = validateToken(token, SECRET);
+    const userID = decodedToken.sub;
+    const user = await User.findById(userID).exec();
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(401).json({ message: "Forbidden" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `Confirmation failed ${error.message}` });
+  }
+};
+
+/**
+ * Fetches a user document depending on the type of credential given, can either be username(alphanumeric) or email
+ * @param req
+ * @param res
+ * @param next
+ */
+export const getUserByCredential = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const credential: string = req.body.credential;
+  try {
+    const user = await User.findOne(
+      credential.includes("@")
+        ? { email: credential }
+        : { username: credential }
+    ).exec();
+    if (!user) {
+      res.status(404).json({ message: "No user with such credentials" });
+      return;
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+  // get credential type
+};
