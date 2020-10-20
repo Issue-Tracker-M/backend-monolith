102ad1fbc371425457d23e81cf3c47013f47bca0
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-20 06:21:31 +0500

    Strict types for models

--- src/models/Workspace.ts
+++ src/models/Workspace.ts
@@ -1,22 +1,22 @@
-import mongoose, { Document } from "mongoose";
-import { ITask } from "./Task";
-import { IUser } from "./User";
+import mongoose, { Document, Types } from "mongoose";
+import { Task } from "./Task";
+import { UserDocument } from "./User";
 
-export interface ILabel extends Document {
+export interface Label {
   name: string;
   color: string;
 }
 
-const Label = new mongoose.Schema({
+const LabelSchema = new mongoose.Schema({
   name: String,
   color: String, //hex color code
 });
 
-export interface IChange extends Document {
-  [key: string]: any;
+export interface Change {
+  [key: string]: unknown;
 }
 
-const Change = new mongoose.Schema({
+const ChangeSchema = new mongoose.Schema({
   //   text: String,
   // what happened // string based on the type of change or a type of possible action
 
@@ -27,25 +27,41 @@ const Change = new mongoose.Schema({
   user: { type: mongoose.Types.ObjectId, ref: "Users" },
 });
 
-export interface IWorkspace extends Document {
+export interface Workspace {
   name: string;
-  labels: ILabel[];
-  users: IUser["_id"][];
-  admin: IUser["_id"];
-  tasks: ITask["_id"][];
-  history: IChange[];
+  labels?: Label[];
+  users?: UserDocument["_id"][] | UserDocument[];
+  admin: UserDocument["_id"] | UserDocument;
+  tasks?: Task["_id"][];
+  history?: Change[];
 }
 
-const Workspaces = mongoose.model(
+interface WorkspaceBaseDocument extends Workspace, Document {
+  labels: Types.Array<Label>;
+  tasks: Types.Array<Task>;
+  history: Types.Array<Change>;
+}
+
+export interface WorkspaceDocument extends WorkspaceBaseDocument {
+  users: Types.Array<UserDocument["_id"]>;
+  admin: UserDocument["_id"];
+}
+
+export interface WorkspacePopulatedDocument extends WorkspaceBaseDocument {
+  users: Types.Array<UserDocument>;
+  admin: UserDocument;
+}
+
+const Workspaces = mongoose.model<WorkspaceDocument>(
   "Workspace",
   new mongoose.Schema(
     {
-      name: String,
-      labels: [Label], //all of labels defined for this workspace
+      name: { type: String, required: true },
+      labels: [LabelSchema], //all of labels defined for this workspace
       users: [{ type: mongoose.Types.ObjectId, ref: "Users" }], //references to all the users
-      admin: { type: mongoose.Types.ObjectId, ref: "Users" },
+      admin: { type: mongoose.Types.ObjectId, ref: "Users", required: true },
       tasks: [{ type: mongoose.Types.ObjectId, ref: "Tasks" }],
-      history: [Change],
+      history: [ChangeSchema],
     },
     { timestamps: true }
   )
