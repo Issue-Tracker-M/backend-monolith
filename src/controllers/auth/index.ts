bfd335725d79b9e64e304ef24a55ddeb5beec1db
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-22 19:23:17 +0500

    Added email confirmation controller

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,7 +1,11 @@
 import User, { User as IUser, UserDocument } from "./../../models/User";
+import { EMAIL_SECRET } from "../../config";
 import { Request, Response } from "express";
 import generateToken from "../../utils/generateToken";
 import bcrypt from "bcrypt";
+import sendMail from "../../utils/sendEmail";
+import confirmEmailTemplate from "../../templates/confirmEmailTemplate";
+import { validateToken } from "../../utils/validateToken";
 
 /**
  * Registers a new user with username, email & password. Sends back the new user document & token.
@@ -19,7 +23,11 @@ export interface registerInput {
 interface RegisterRequest extends Request {
   body: registerInput;
 }
-
+/**
+ * Creates a new user and sends an email confirmation letter.
+ * @param req
+ * @param res
+ */
 export const register = async (
   req: RegisterRequest,
   res: Response
@@ -27,7 +35,6 @@ export const register = async (
   try {
     const { first_name, last_name, username, password, email } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = new User({
       email,
       password: hashedPassword,
@@ -38,23 +45,41 @@ export const register = async (
 
     const user = await newUser.save();
 
-    const token = generateToken(user);
+    const token = generateToken(user, EMAIL_SECRET);
 
-    res.status(201).json({ token, user });
+    sendMail({
+      subject: "Welcome to Issue Tracker!",
+      to: email,
+      html: confirmEmailTemplate(first_name + last_name, token),
+    });
+
+    res.status(201).json();
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
+/**
+ * Expected input for the regular login endpoint without Oauth.
+ */
 export interface loginInput {
   credential: IUser["username"] | IUser["email"];
   password: IUser["password"];
 }
 
+/**
+ * Regular login requests have the user document with matching credential attached to req.user.
+ */
 interface LoginRequest extends Request {
   body: loginInput;
+  user: UserDocument;
 }
 
+/**
+ * Checks the validity of given credentials and issues a JWT.
+ * @param req
+ * @param res
+ */
 export const login = async (
   req: LoginRequest,
   res: Response
@@ -62,15 +87,41 @@ export const login = async (
   try {
     const validPassword = await bcrypt.compare(
       req.body.password,
-      (req.user as IUser).password
+      req.user.password
     );
     if (validPassword) {
-      const token = generateToken(req.user as UserDocument);
+      const token = generateToken(req.user);
       res.status(200).json({ token });
     } else {
       res.status(401).json({ message: "Invalid credentials" });
     }
   } catch (error) {
     res.status(500).json({ message: "Failed to login" });
+  }
+};
+
+interface ConfirmEmailRequest extends Request {
+  body: {
+    token: string;
+  };
+}
+export const confirmEmail = async (
+  req: ConfirmEmailRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { sub } = validateToken(req.body.token, EMAIL_SECRET);
+    const user = await User.findOneAndUpdate(
+      { _id: sub },
+      { is_verified: true }
+    ).exec();
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    const token = generateToken(user);
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ message: error });
   }
 };
