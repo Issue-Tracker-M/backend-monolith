a46f553358a3eff817868a9173f63c21666e1e44
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-27 15:00:11 +0500

    Fixed token models to be indexable by token

--- src/models/ConfirmationToken.ts
+++ src/models/ConfirmationToken.ts
@@ -7,8 +7,8 @@ export interface ConfirmationToken {
 
 const tokenSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-  token: { type: String, required: true },
-  createdAt: { type: Date.now, required: true, expires: 43200 },
+  token: { type: String, required: true, unique: true },
+  createdAt: { type: Date, required: true, expires: 43200, default: Date.now },
 });
 
 export interface ConfirmationTokenDocument
--- src/models/PasswordResetToken.ts
+++ src/models/PasswordResetToken.ts
@@ -7,8 +7,8 @@ export interface PasswordResetToken {
 
 const tokenSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-  token: { type: String, required: true },
-  createdAt: { type: Date.now, required: true, expires: 43200 },
+  token: { type: String, required: true, unique: true },
+  createdAt: { type: Date, required: true, expires: 43200, default: Date.now },
 });
 
 export interface PasswordTokenDocument extends PasswordResetToken, Document {}
