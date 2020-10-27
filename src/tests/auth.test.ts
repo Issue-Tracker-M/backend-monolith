65f7fac68a5b89c6b9e37b0c2b359d3f7438a9f9
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-27 15:00:43 +0500

    Email confirmation tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,6 +2,7 @@ import { registerInput } from "../controllers/auth";
 import app from "../api/app";
 import supertest from "supertest";
 import User from "../models/User";
+const iwm: any = require("nodemailer-stub").interactsWithMail;
 
 beforeAll(async (done) => {
   try {
@@ -21,15 +22,33 @@ const newUser: registerInput = {
 };
 
 describe("Auth", () => {
-  it("User can register with username, email, and password", async (done) => {
+  it("User can register with username, email, and password. And then confirm their email", async (done) => {
+    // Create user and check that it creates correctly
     const res = await supertest(app).post("/api/auth/register").send(newUser);
-    expect(res.body).toEqual(
+    expect(res.status).toBe(201);
+    // wait for the mail to be put into iwm
+    await new Promise((resolve, reject) => {
+      setInterval(() => resolve(1), 50);
+    });
+    // get token from the email
+    const email_token = iwm
+      .lastMail()
+      .content.split("/confirm/")[1]
+      .split(`" target`)[0];
+    // follow the verification link
+    const verification_res = await supertest(app)
+      .post("/api/auth/confirm_email")
+      .send({ token: email_token });
+    // check the response contents to be the right shape
+    expect(verification_res.body).toEqual(
       expect.objectContaining({
         token: expect.any(String),
         user: expect.any(Object),
       })
     );
-    expect(res.status).toBe(201);
+    // check the right status
+    expect(verification_res.status).toBe(200);
+
     done();
   });
 
