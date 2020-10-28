b8d05e941e242c72a377b8e20c599774707d3741
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-28 12:03:52 +0500

    Refactor to get mongoose models registered correct

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,13 +1,29 @@
 import { port, mongoURI } from "./../config/index";
 import express from "express";
 import mongoose from "mongoose";
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((conn) =>
+    console.log(
+      `MongoDB connection with url successful @: ${conn.connection.host}`
+    )
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+require("../models");
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
+import authRouter from "../routes/authRouter";
+import taskRouter from "../routes/taskRouter";
+import workspaceRouter from "../routes/workspaceRouter";
 
-const authRouter = require("../routes/authRouter");
-const taskRouter = require("../routes/taskRouter");
-const workspaceRouter = require("../routes/workspaceRouter");
 const apiRouter = express.Router();
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/tasks", taskRouter);
@@ -21,17 +37,6 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 app.use("/api", apiRouter);
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-  .then((conn) =>
-    console.log(
-      `MongoDB connection with url successful @: ${conn.connection.host}`
-    )
-  )
-  .catch((err) => {
-    console.log(err);
-  });
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "API is up 🚀" });
