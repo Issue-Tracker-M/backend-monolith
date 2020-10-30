0a63095096d4358aee3df9be0749302589bb3fdc
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-10-30 11:21:52 +0500

    Password reset tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,6 +1,6 @@
 import app from "../api/app";
 import supertest from "supertest";
-import { clearDB, newUser } from "./test_utils";
+import { clearDB, createUser, newUser } from "./test_utils";
 /* 
 iwm is a singleton used by nodemailer-stub to store all of the newly created emails in memory
 and give you easy access to them for testing  purposes
@@ -68,6 +68,51 @@ describe("Auth", () => {
       expect.objectContaining({ token: expect.any(String) })
     );
     expect(res.status).toBe(200);
+    done();
+  });
+
+  it("User can reset password", async (done) => {
+    // create a user
+    // send a forgot password request
+    // get password reset token from email
+    // send password reset token & new password to reset_password endpoint
+    // check that the user can log in with the new password
+    await clearDB();
+    const { user, token } = await createUser();
+    iwm.flushMails();
+    const res = await supertest(app)
+      .post("/api/auth/forgot_password")
+      .send({ email: user.email });
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: "Password reset mail sent to user email",
+    });
+    // await new Promise((resolve, reject) => {
+    //   setInterval(() => resolve(1), 50);
+    // });
+    // get token from the email
+    const reset_token = iwm
+      .lastMail()
+      .content.split("/reset/")[1]
+      .split(`" target`)[0];
+    const newPassword = "a fine day in typescript 4.0 heaven";
+    const res2 = await supertest(app).post("/api/auth/reset_password").send({
+      token: reset_token,
+      password: newPassword,
+      repeat_password: newPassword,
+    });
+    expect(res2.status).toBe(200);
+    expect(res2.body).toEqual({ message: "Password updated" });
+
+    // try to login using updated password
+    const res3 = await supertest(app)
+      .post("/api/auth/login")
+      .send({ credential: user.email, password: newPassword });
+
+    expect(res3.body).toEqual(
+      expect.objectContaining({ token: expect.any(String) })
+    );
+    expect(res3.status).toBe(200);
     done();
   });
 });
