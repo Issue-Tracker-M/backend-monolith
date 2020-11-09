5e28dd07f37411014a73178bbd0c863d2c151a65
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-11-09 16:27:07 +0500

    Protected the task route

--- src/routes/taskRouter.ts
+++ src/routes/taskRouter.ts
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+import { checkToken } from "../controllers/auth/middleware";
 import { createTask } from "../controllers/tasks";
 
-router.post("/", createTask);
+router.post("/", checkToken, createTask);
 
 export default router;
