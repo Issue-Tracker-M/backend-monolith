a20bd017e8cc5009059756fca9a0fbddee7ca8ca
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2020-11-17 09:03:31 +0500

    Task removed from workspace upon deletion

--- src/controllers/tasks/deleteTask.ts
+++ src/controllers/tasks/deleteTask.ts
@@ -1,5 +1,6 @@
 import { Response } from "express";
 import Tasks from "../../models/Task";
+import Workspaces from "../../models/Workspace";
 import { AuthorizedRequest } from "../auth/middleware";
 
 export async function deleteTask(
@@ -15,6 +16,12 @@ export async function deleteTask(
       return;
     }
     await Tasks.deleteOne({ _id: taskId });
+    const w = await Workspaces.findById(task.workspace).exec();
+    if (!w) throw new Error("Workspace not found");
+    w.todo.pull(task.id);
+    w.in_progress.pull(task.id);
+    w.completed.pull(task.id);
+    await w.save();
     res.status(204).json({ message: "Task has been deleted" });
     return;
   } catch (error) {
